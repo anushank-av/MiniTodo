41c7eb03da0a1085809b963c60edb260658b6046
anushank-av <falconmoto1104@gmail.com>
2017-02-16 17:54:58 +0530

    Added heler and contract classes

---
+++ app/src/main/java/in/avprojects/minitodo/database/TodoContract.java
@@ -0,0 +1,71 @@
+package in.avprojects.minitodo.database;
+
+import android.content.ContentResolver;
+import android.net.Uri;
+import android.provider.BaseColumns;
+
+/**
+ * Created by anush on 16-02-2017.
+ */
+
+public final class TodoContract {
+    // Declaration of the content authority
+
+    public TodoContract(){}
+
+    public static final String CONTENT_AUTHORITY = "in.avprojects.minitodo";
+
+
+    //Base Uri for all Uri'
+    public static final Uri BASE_URI = Uri.parse("content://"+CONTENT_AUTHORITY);
+
+    //The name of the table ( for external purposes only )
+
+    public static final String PATH_TO_TODO = "todos";
+
+    public static final class TodoTable implements BaseColumns {
+        // Some normal declarations
+        //Table name
+
+        public static final String TABLE_NAME = "todos";
+
+        //BAse Uri for this table
+        public Uri TABLE_URI = Uri.withAppendedPath(BASE_URI,PATH_TO_TODO);
+
+        //Table attributes;
+
+        //Mimetype for list of todos
+        public static final String CONTENT_DIR_TYPE = ContentResolver.CURSOR_DIR_BASE_TYPE+"/"+CONTENT_AUTHORITY+"/"+PATH_TO_TODO;
+        //Mime type for single todotask
+        public static final String CONTENT_BASE_TYPE = ContentResolver.CURSOR_ITEM_BASE_TYPE+"/"+CONTENT_AUTHORITY+"/"+PATH_TO_TODO;
+
+        //ID Column
+        public static final String ID = BaseColumns._ID;
+
+        //Title column
+
+        public static final String COLUMN_TITLE = "title";
+
+        //Priority Column
+
+        public static final String COLUMN_PRIORITY = "priority";
+
+        //Description column
+
+        public static final String COLUMN_DESCRIPTION = "description";
+
+        //Possible values for the priority column
+
+        public static final int PRIORITY_LOW = 0;
+        public static final int PRIORITY_MED = 1;
+        public static final int PRIORITY_HIGH = 2;
+
+        public boolean isValidPriority(int pr){
+            if (pr == PRIORITY_HIGH||pr == PRIORITY_LOW||pr == PRIORITY_MED) return true;
+            else return false;
+        }
+
+
+    }
+
+}
---
+++ app/src/main/java/in/avprojects/minitodo/database/TodoHelper.java
@@ -0,0 +1,41 @@
+package in.avprojects.minitodo.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import static in.avprojects.minitodo.database.TodoContract.*;
+
+/**
+ * Created by anush on 16-02-2017.
+ */
+
+public class TodoHelper extends SQLiteOpenHelper {
+
+    // The database name
+
+    public static final String DB_NAME = "tododatabase.db";
+
+    //The database version
+
+    public static final int DB_VERSION = 1;
+    //The SQL Instruction to ceate a database
+    public static final String CREATE_TABLE = "create table " + TodoTable.TABLE_NAME + "("
+            +TodoTable.ID + " integer primary key autoincrement,"
+            +TodoTable.COLUMN_TITLE + "text not null"
+            +TodoTable.COLUMN_PRIORITY+" integer default 0,"
+            +TodoTable.COLUMN_DESCRIPTION+" text"+");";
+    public TodoHelper(Context context) {
+        super(context, DB_NAME, null, DB_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(CREATE_TABLE);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
