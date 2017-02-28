5989c2784b65161c13a33868c298f72cfe3f7227
anushank_av <falconmoto1104@gmail.com>
2017-02-28 15:28:57 +0530

    Minor Layout Adjustments

--- app/src/main/java/in/avprojects/minitodo/EditorActivity.java
+++ app/src/main/java/in/avprojects/minitodo/EditorActivity.java
@@ -4,8 +4,17 @@ package in.avprojects.minitodo;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
 
 public class EditorActivity extends AppCompatActivity {
+    private EditText titletext;
+
+    private EditText descriptionEditText;
+
+    private Spinner prioritySpinner;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,5 +24,17 @@ public class EditorActivity extends AppCompatActivity {
         actionBar.setDisplayHomeAsUpEnabled(true);
 
 
+        titletext.findViewById(R.id.edit_title);
+        descriptionEditText.findViewById(R.id.edit_details);
+        setupSpinner();
+
+
+    }
+
+    private void setupSpinner() {
+        ArrayAdapter<CharSequence> spinnerItems = ArrayAdapter.createFromResource(this,R.array.priorities,android.R.layout.simple_spinner_item);
+        spinnerItems.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        prioritySpinner = (Spinner)findViewById(R.id.edit_priority);
+        prioritySpinner.setAdapter(spinnerItems);
     }
 }
